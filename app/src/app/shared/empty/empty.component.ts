35984e3d4245086923e0231109b009a4eae6c8b9
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-08-31 20:07:55 +0100

    ft/add ocr and animations (#68)
    * ft: add fadeIn and fadeInUp animations for smoother transitions
    
    * ft: add OCR functionality and animations for book scanning feature

--- app/src/app/shared/empty/empty.component.ts
+++ app/src/app/shared/empty/empty.component.ts
@@ -2,12 +2,14 @@ import { Component, Input } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { fileTrayOutline } from 'ionicons/icons';
+import { fadeIn } from '../../animations';
 
 @Component ({
   selector: 'app-empty',
   standalone: true,
+  animations: [ fadeIn ],
   template: `
-    <div class="empty">
+    <div class="empty" [@fadeIn]>
       <ion-icon name="file-tray-outline" size="large"></ion-icon>
       <div class="message">{{ message }}</div>
     </div>
